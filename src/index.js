5614b895f3b64116f55f17a54aab2663dd2ebddb
Duc Le <minhducle1411@gmail.com>
2023-07-23 20:13:04 +0700

    SCSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// npm start
+
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
@@ -5,6 +7,9 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+
+// khai báo đường dẫn file hình
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
 app.use(morgan('combined'));
